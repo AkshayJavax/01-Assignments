3c49789fe7bf4cfdfe469dd7274f4c749e1a9e0f
Akshay <68456478+AkshayJavax@users.noreply.github.com>
2020-11-22 22:32:24 +0530

    Add files via upload

---
+++ StudentBo.java
@@ -0,0 +1,19 @@
+  package e.com.nt;
+  public class StudentBo{
+  private int no;
+  private String sname;
+  public void setNo(int no){
+  this.no=no;
+  }
+  public int getNo(){
+  return no;
+
+  }
+  public void setSname(String sname){
+  this.sname=sname;
+  }
+  public String getSname(){
+  return sname;
+  }
+
+}
